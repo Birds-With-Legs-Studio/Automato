96240018a69873c3ec569233fa1e38c5fd367606
iosenberg <70673719+iosenberg@users.noreply.github.com>
2025-10-31 15:19:32 -0400

    Add local player tile interaction

--- src/player/player.gd
+++ src/player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var speed = 40 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
 var environment
+var camera
 #0 = left, 1 = up, 2 = right, 3 = down
 #TODO: There's GOT to be a better way to do thi
 var direction : int
@@ -12,6 +13,7 @@ var direction : int
 func _ready():
 	screen_size = get_viewport_rect().size
 	environment = get_node("../TileMap/TileMapLayer2")
+	camera = get_node("Camera2D")
 	direction = 3
 
 
@@ -20,10 +22,12 @@ func _physics_process(_delta):
 	# Get player's movement vector from input
 	velocity = Input.get_vector("left", "right", "up", "down")
 	
-	# Determine player direction based on movement
+	# Determine player direction based on movement (prefer horizontal)
 	if velocity.x != 0:
+		@warning_ignore("narrowing_conversion")
 		direction = velocity.x + 1
 	elif velocity.y != 0:
+		@warning_ignore("narrowing_conversion")
 		direction = velocity.y + 2
 	
 	# Set velocity to normalized vector and move player
@@ -60,13 +64,27 @@ func _physics_process(_delta):
 
 #Process mouse presses
 func _input(event):
-	#If it's a mouse event
+	#If it's a mouse event, process click
 	if event is InputEventMouseButton and event.pressed:
-		var place_vector = environment.local_to_map(position)
-		if direction % 2 == 0:
-			place_vector += Vector2i(direction - 1, 0)
+		# TODO: This is kind of an unideal way to get mouse position. Figure out later
+		var place_vector = environment.local_to_map(camera.get_global_mouse_position())
+		
+		var d_from_player = place_vector - environment.local_to_map(position)
+		# If clicked location is within 1 cell of player, change direction accordingly (prefer verical)
+		if int(d_from_player.length()) == 1:
+			if d_from_player.y != 0:
+				direction = d_from_player.y + 2
+			else:
+				direction = d_from_player.x + 1
+		# Otherwise, change place vector to cell in front of player
 		else:
-			place_vector += Vector2i(0, direction - 2)
+			place_vector = environment.local_to_map(position)
+			if direction % 2 == 0:
+				place_vector += Vector2i(direction - 1, 0)
+			else:
+				place_vector += Vector2i(0, direction - 2)
+		
+		# Left button = place, right button = destroy
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			environment.set_cell(place_vector, 0, Vector2(0,0), 0)
 		if event.button_index == MOUSE_BUTTON_RIGHT:
