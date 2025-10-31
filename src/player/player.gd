ec656a32a6419bb8e2d110e3d5ae088d0d0a2ad4
iosenberg <70673719+iosenberg@users.noreply.github.com>
2025-10-31 13:32:58 -0400

    Move click interactions to events instead of input poll

--- src/player/player.gd
+++ src/player/player.gd
@@ -11,9 +11,9 @@ func _ready():
 	environment = get_node("../TileMap/TileMapLayer2")
 	facing = 'd'
 	
-	
-func _process(_delta):
-	#Process player movement
+
+#Process player movement
+func _physics_process(_delta):
 	velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
@@ -62,10 +62,10 @@ func _process(_delta):
 				$AnimatedSprite2D.animation = "stand_down"
 				$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.stop()
-	
-		
-	#Process player interaction
-	if Input.is_action_pressed("Left Interact"):
+
+#Process mouse presses
+func _input(event):
+	if event.is_action_pressed("Left Interact"):
 		var facing_vector
 		match facing:
 			'r':
@@ -78,7 +78,7 @@ func _process(_delta):
 				facing_vector = Vector2i(0,1)
 		environment.set_cell(environment.local_to_map(position) + facing_vector, 0, Vector2(0,0), 0)
 		
-	if Input.is_action_pressed("Right Interact"):
+	if event.is_action_pressed("Right Interact"):
 		var facing_vector
 		match facing:
 			'r':
@@ -90,5 +90,3 @@ func _process(_delta):
 			'd':
 				facing_vector = Vector2i(0,1)
 		environment.set_cell(environment.local_to_map(position) + facing_vector, 0, Vector2(0,1), 0)
-
-	
